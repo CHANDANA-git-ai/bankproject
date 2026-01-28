cacf79e11f807a98c822f2b26b35faf4b47cbe72
CHANDANA-git-ai <chandanachari.k@gmail.com>
2026-01-28 21:43:06 +0530

    Update mysql.sql

--- mysql.sql
+++ mysql.sql
@@ -2,10 +2,12 @@ CREATE DATABASE bank_db;
 USE bank_db;
 CREATE TABLE customers (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(100) NOT NULL,
+    name VARCHAR(100),
     email VARCHAR(100),
     mobile VARCHAR(15),
-    city VARCHAR(50)
+    city VARCHAR(50),
+    balance DOUBLE,
+    status VARCHAR(20)
 );
 DESC customers;
 select * from customers;
