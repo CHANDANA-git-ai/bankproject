48e1b46c264e2436117ea479553c832133217fd6
CHANDANA-git-ai <chandanachari.k@gmail.com>
2026-01-28 21:37:50 +0530

    Update CustomerApp.java

--- CustomerApp.java
+++ CustomerApp.java
@@ -4,44 +4,40 @@ import java.util.Scanner;
 
 /*
  * Main class
- * Entry point of the application
- * Displays menu and calls service methods
+ * Entry point of application
  */
 public class CustomerApp {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
-        CustomerService service = new CustomerService();
         Scanner sc = new Scanner(System.in);
+        CustomerService service = new CustomerService();
 
         while (true) {
-            System.out.println("\n--- CUSTOMER MENU ---");
-            System.out.println("1. Add Customer");
+            System.out.println("\n=== SMART BANK MANAGEMENT SYSTEM ===");
+            System.out.println("1. Create Account");
             System.out.println("2. View Customers");
-            System.out.println("3. Search Customer by ID");
-            System.out.println("4. Update Customer Mobile");
-            System.out.println("5. Delete Customer");
-            System.out.println("6. Customer Count");
-            System.out.println("7. View Customers by City");
-            System.out.println("8. Exit");
-            System.out.print("Enter choice: ");
+            System.out.println("3. Update Customer");
+            System.out.println("4. Delete Customer");
+            System.out.println("5. Deposit Money");
+            System.out.println("6. Withdraw Money");
+            System.out.println("7. Exit");
+            System.out.print("Choose Option: ");
 
-            int choice = sc.nextInt();
-            sc.nextLine(); // consume newline
+            int choice = Integer.parseInt(sc.nextLine());
 
             switch (choice) {
-                case 1 -> service.addCustomer();
+                case 1 -> service.createCustomer();
                 case 2 -> service.showCustomers();
-                case 3 -> service.searchCustomer();
-                case 4 -> service.updateMobile();
-                case 5 -> service.deleteCustomer();
-                case 6 -> service.showCustomerCount();
-                case 7 -> service.showCustomersByCity();
-                case 8 -> {
-                    System.out.println("Thank you!");
+                case 3 -> service.updateCustomer();
+                case 4 -> service.deleteCustomer();
+                case 5 -> service.depositMoney();
+                case 6 -> service.withdrawMoney();
+                case 7 -> {
+                    System.out.println("Thank you for using Smart Bank System");
                     return;
                 }
-                default -> System.out.println("Invalid choice");
+                default -> System.out.println("Invalid Choice");
             }
         }
     }
